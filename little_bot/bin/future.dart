d324cb9eff3e6261edfdcbecaeb35fec5ad0b2df
alvaronogueira <alvaronogueira29@gmail.com>
2023-03-30 21:20:32 -0300

    update future.dart

---
+++ little_bot/bin/future.dart
@@ -0,0 +1,16 @@
+void main(){
+  Future myFuture = Future(() {
+    print('Going back to the Future.');
+    return 59;
+  }).then((value) => print('The value is $value'));
+
+  Future<int> myFutureFunc() async {
+    print('I have a function in the Future.');
+    await Future.delayed(Duration(seconds: 5));
+    return 12;
+  }
+
+  myFutureFunc().then((value) => print('My Function value is: $value.'));
+
+  print('Done with main.');
+}
--- little_bot/bin/little_bot.dart
+++ little_bot/bin/little_bot.dart
@@ -1,5 +1,5 @@
 import 'package:little_bot/little_bot.dart' as little_bot;
 
 void main() {
-  print('Hello world: ${little_bot.calculate()}!');
+  print('Hello world');
 }
