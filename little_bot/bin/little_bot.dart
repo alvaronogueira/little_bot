6cdf96a0e40dd6a6465a288606aa8984d9becd0d
alvaronogueira <alvaronogueira29@gmail.com>
2023-03-30 21:14:29 -0300

    Initial commit

---
+++ little_bot/bin/little_bot.dart
@@ -0,0 +1,5 @@
+import 'package:little_bot/little_bot.dart' as little_bot;
+
+void main() {
+  print('Hello world: ${little_bot.calculate()}!');
+}
---
+++ little_bot/test/little_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:little_bot/little_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
