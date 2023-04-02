6a4485becddff8c0ef3dac80b20732fa05abf7cb
alvaronogueira <alvaronogueira29@gmail.com>
2023-04-02 15:28:16 -0300

    up future.dart

--- little_bot/bin/future.dart
+++ little_bot/bin/future.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:test/expect.dart';
 
 void main() {
@@ -13,10 +15,27 @@ void main() {
     return 12;
   }
 
-  myFutureFunc()
-      .then((value) => print('My Function value is: $value.'))
-      .onError((error, stackTrace) => print('An error occurred'))
-      .whenComplete(() => print('The future is over now!'));
+  //myFutureFunc()
+  //    .then((value) => print('My Function value is: $value.'))
+  //    .onError((error, stackTrace) => print('An error occurred'))
+  //    .whenComplete(() => print('The future is over.'));
 
+  Future<int> myFutureErrorFunc(int a, int b) async {
+    try {
+      if (a > b) {
+        throw Exception();
+      }
+      print('I have a functional Function');
+      await Future.delayed(Duration(seconds: 5));
+      return 46;
+    } catch (e) {
+      print('An error occurred: $e');
+      return 46;
+    } finally {
+      print('The future is finally over.');
+    }
+  }
+
+  myFutureErrorFunc(2, 3).then((value) => print('The value is $value'));
   print('Done with main.');
 }
