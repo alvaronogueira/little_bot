f211ef032a58f7ed46e8a79b0ecf7a85dabeae65
alvaronogueira <alvaronogueira29@gmail.com>
2023-04-04 20:27:19 -0300

    update lilbot.dart

--- little_bot/bin/lil_bot.dart
+++ little_bot/bin/lil_bot.dart
@@ -1,43 +1,44 @@
 import 'questions/time_questions.dart';
+import 'timing/waiting_time.dart';
 import 'dart:io';
+
 
 void main() async {
   String lilbot = 'LilBOT:\n';
   var a = true;
-  String user = '';
+  String usuario = '';
 
   print('-- Starting LilBOT, wait --');
+  await BotClock().clock(2);
   print('-- LilBOT:\n Hello! :)\n How can I help You? --');
 
-  // deleted code
   do {
-    user = stdin.readLineSync().toString();
+    usuario = stdin.readLineSync().toString();
     print('-- processing the question, wait --');
-    
-    if (
-      user.contains('Bye') ||
-      user.contains('Bye') ||
-      user.contains('GoodBye') ||
-      user.contains('GoodBye')) {
-        a = false;
-        print(lilbot + 'See Ya!');
-  } // deleted code
-        
-  } else if (TimeQuestions(user).isThisTime()) {
-    //check before, this way we not do all function 
-    TimeQuestions(user).timeQuestion();
-    // ignore: dead_code
-   } else if (false) {
-    //ADD news questions here
-  } else {
+    await BotClock().clock(1);
+    if (usuario.contains('Bye') ||
+        usuario.contains('Bye') ||
+        usuario.contains('GoodBye') ||
+        usuario.contains('GoodBye')) {
+      a = false;
+      print(lilbot + 'See Ya!');
+    } else if (TimeQuestions(usuario).isThisTime()) {
+      //check before, this way we not do all function
+      await BotClock().clock(2); 
+      TimeQuestions(usuario).timeQuestion();
+    } else if (false) {
+      //ADD news questions here
+    } else {
+      await BotClock().clock(2);
       print(lilbot + 
-        'I am not abled to answer this questions\n I am sorry :('); 
+        'I am not abled to answer this questions\n I am sorry :(');
       print(lilbot + 'You could doing other question or say bye');
+    }
   } while (a);
 
   print('-- shutting down LilBot --');
-  
 }
+
 
 
 
