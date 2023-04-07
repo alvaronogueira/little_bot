844665603208f9b5e74ed60cf3c59cfc61ba45de
alvaronogueira <alvaronogueira29@gmail.com>
2023-04-06 22:31:55 -0300

    update good_manners.dart

--- little_bot/bin/questions/good_manners.dart
+++ little_bot/bin/questions/good_manners.dart
@@ -9,18 +9,43 @@ class GoodManners {
         question.contains('hello') ||
         question.contains('Hi') ||
         question.contains('Hello') ||
-        question.contains('obrigado') ||
-        question.contains('obrigada') ||
-        question.contains('Obrigado') ||
-        question.contains('Obrigada') ||
-        question.contains('bom') ||
-        question.contains('boa') ||
-        question.contains('tarde') ||
-        question.contains('noite') ||
-        question.contains('de nada') ||
-        question.contains('De nada')) {
+        question.contains('thanks') ||
+        question.contains('thank you') ||
+        question.contains('Thank you') ||
+        question.contains('Thank You') ||
+        question.contains('good') ||
+        question.contains('Good') ||
+        question.contains('afternoon') ||
+        question.contains('Afternoon') ||
+        question.contains('evening') ||
+        question.contains('Evening') ||
+        question.contains('you`re welcome') ||
+        question.contains('You`re welcome')) {
       isThisGoodManners = true;
     }
     return isThisGoodManners;
+  }
+
+  goodManners() {
+    if (question.contains('good morning') ||
+        question.contains('Good morning')) {
+      print(lilBot + 'Good morning, Smile');
+    } else if (question.contains('good afternoon') ||
+        question.contains('Good afternoon')) {
+      print('Oops, good afternoon');
+    } else if (question.contains('good evening') ||
+        question.contains('Good evening')) {
+      print('Good evening, right?');
+    } else if (question.contains('hi') ||
+        question.contains('hello') ||
+        question.contains('Hi') ||
+        question.contains('Hello')) {
+      print(lilBot + 'Its a pleasure');
+    } else if (question.contains('thanks') ||
+        question.contains('thank you') ||
+        question.contains('Thank you') ||
+        question.contains('Thank You')) {
+      print('No problems, friend');
+    }
   }
 }
