a1d7382030a01d6685e44c253939fbd7d54f51cb
alvaronogueira <alvaronogueira29@gmail.com>
2023-04-06 21:04:06 -0300

    update straems.dart

--- little_bot/bin/streams.dart
+++ little_bot/bin/streams.dart
@@ -1,17 +1,26 @@
+import 'dart:async';
+import 'dart:math';
+
 void main() {
   Stream myStream(int interval, [int? maxCount]) async* {
     int i = 1;
-
     while (i != maxCount) {
       print('Couting: $i');
       await Future.delayed(Duration(seconds: interval));
-      i = i + 1;
       yield i++;
     }
-    print('The Stream is finished');
+    print('The Stream is finished.');
   }
 
+  StreamSubscription mySubscriber = myStream(1, 10).listen((event) {
+    if (event.isEven) {
+      print('This number is even!');
+    }
+  }, onError: (e) {
+    print('An error happennd: $e');
+  }, onDone: () {
+    print('Subscriber is gone.');
+  });
 
-  myStream(1, 10);
-  print('The Main finished');
+  print('The Main is finished.');
 }
